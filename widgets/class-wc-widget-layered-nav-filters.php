2b109f5300673cf9bf5de93df5ac2fec8f72108f
fregire <fregire@mail.ru>
2019-06-18 20:11:27 +0500

    Добавил title для очищения фильтров

--- widgets/class-wc-widget-layered-nav-filters.php
+++ widgets/class-wc-widget-layered-nav-filters.php
@@ -90,7 +90,7 @@ class Custom_WC_Widget_Layered_Nav_Filters extends WC_Widget_Layered_Nav_Filters
 							$link = add_query_arg( $filter_name, implode( ',', $new_filter ), $link );
 						}
 
-						echo '<li class="chosen"><a rel="nofollow" aria-label="' . esc_attr__( 'Remove filter', 'woocommerce' ) . '" href="' . esc_url( $link ) . '">' . $cross_icon . ' ' . esc_html( $term->name ) . '</a></li>';
+						echo '<li class="chosen"><a rel="nofollow" title="' . esc_attr__( 'Remove filter', 'woocommerce' ) . '" aria-label="' . esc_attr__( 'Remove filter', 'woocommerce' ) . '" href="' . esc_url( $link ) . '">' . $cross_icon . ' ' . esc_html( $term->name ) . '</a></li>';
 					}
 				}
 			}
@@ -98,13 +98,13 @@ class Custom_WC_Widget_Layered_Nav_Filters extends WC_Widget_Layered_Nav_Filters
 			if ( $min_price ) {
 				$link = remove_query_arg( 'min_price', $base_link );
 				/* translators: %s: minimum price */
-				echo '<li class="chosen"><a rel="nofollow" aria-label="' . esc_attr__( 'Remove filter', 'woocommerce' ) . '" href="' . esc_url( $link ) . '">' . $cross_icon . sprintf( __( 'Min %s', 'woocommerce' ), wc_price( $min_price ) ) . '</a></li>'; // WPCS: XSS ok.
+				echo '<li class="chosen"><a rel="nofollow" title="' . esc_attr__( 'Remove filter', 'woocommerce' ) . '"aria-label="' . esc_attr__( 'Remove filter', 'woocommerce' ) . '" href="' . esc_url( $link ) . '">' . $cross_icon . sprintf( __( 'Min %s', 'woocommerce' ), wc_price( $min_price ) ) . '</a></li>'; // WPCS: XSS ok.
 			}
 
 			if ( $max_price ) {
 				$link = remove_query_arg( 'max_price', $base_link );
 				/* translators: %s: maximum price */
-				echo '<li class="chosen"><a rel="nofollow" aria-label="' . esc_attr__( 'Remove filter', 'woocommerce' ) . '" href="' . esc_url( $link ) . '">' . $cross_icon . sprintf( __( 'Max %s', 'woocommerce' ), wc_price( $max_price ) ) . '</a></li>'; // WPCS: XSS ok.
+				echo '<li class="chosen"><a rel="nofollow" title="' . esc_attr__( 'Remove filter', 'woocommerce' ) . '" aria-label="' . esc_attr__( 'Remove filter', 'woocommerce' ) . '" href="' . esc_url( $link ) . '">' . $cross_icon . sprintf( __( 'Max  %s ', 'woocommerce' ), wc_price( $max_price ) ) . '</a></li>'; // WPCS: XSS ok.
 			}
 
 			if ( ! empty( $rating_filter ) ) {
