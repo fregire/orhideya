06deb32e8c5aa7c511fb818401935bc5c86b1968
fregire <fregire@mail.ru>
2019-07-01 13:00:11 +0500

    Добавил код отслеживания google

--- header.php
+++ header.php
@@ -32,6 +32,19 @@
 	</script>
 	<noscript><div><img src="https://mc.yandex.ru/watch/54252694" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 	<!-- /Yandex.Metrika counter -->
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-112316680-3"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-112316680-3');
+	</script>
+
+	<!-- /Google Analytics -->
+
 	<?php wp_head(); ?>
 </head>
 
